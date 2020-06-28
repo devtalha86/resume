a7bc922765fb32abeb1f2c28edc601f5e8e486b1
devtalha86 <39130115+devtalha86@users.noreply.github.com>
2020-06-28 19:29:05 +0600

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,5 @@
 
 
-jQuery(document).ready(function(){
-	jQuery('.skillbar').each(function(){
-		jQuery(this).find('.skillbar-bar').animate({
-			width:jQuery(this).attr('data-percent')
-		},6000);
-	});
-});
 
 
 // Portfolio starts here
